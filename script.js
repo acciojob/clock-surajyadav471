dd3344cdbc9e524a048433bb8991ec93bccd2800
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-05-22 15:49:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function updateClock(){
+	let now=new Date();
+            let output=now.getMonth()+"/"+now.getDate()+"/"+now.getFullYear()+", "+now.getHours()+":"+now.getMinutes()+":"+now.getSeconds();
+            let timer=document.getElementById("timer");
+            timer.innerHTML=output;
+            setTimeout(updateClock,1000);
+		}
+
+		updateClock();
